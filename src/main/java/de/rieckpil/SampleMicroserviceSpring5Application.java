c3303d448f4e19996258e8cae65859235b2c3cbc
Philip Riecks <mail@philipriecks.de>
2017-12-31 17:21:42 +0100

    add initial Spring 5 project

---
+++ src/main/java/de/rieckpil/SampleMicroserviceSpring5Application.java
@@ -0,0 +1,12 @@
+package de.rieckpil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMicroserviceSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleMicroserviceSpring5Application.class, args);
+	}
+}
