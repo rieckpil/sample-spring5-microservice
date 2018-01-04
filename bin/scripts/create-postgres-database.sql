31b3de5660a7eaa6ce1abe028d30ffb2de3d44d0
Philip Riecks <mail@philipriecks.de>
2018-01-04 19:09:18 +0100

    remove untracked files

--- bin/scripts/create-postgres-database.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE sample_application_dev;
-CREATE DATABASE sample_application_prod;
-
-CREATE USER sample_dev_user WITH PASSWORD 'changeme';
-CREATE USER sample_dev_prod WITH PASSWORD 'changeme';
-
-GRANT ALL PRIVILEGES ON DATABASE sample_application_dev to sample_dev_user;
-GRANT ALL PRIVILEGES ON DATABASE sample_application_prod to sample_dev_prod;
