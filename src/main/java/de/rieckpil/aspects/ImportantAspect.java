3e53855934d30017dd3bbd93940dbe6a70427dbc
Philip Riecks <philip@riecks.io>
2018-01-11 20:06:49 +0100

    add join points to logging aspect

--- src/main/java/de/rieckpil/aspects/ImportantAspect.java
+++ src/main/java/de/rieckpil/aspects/ImportantAspect.java
@@ -1,9 +1,11 @@
 package de.rieckpil.aspects;
 
 import java.util.Date;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +18,20 @@ public class ImportantAspect {
 
   @Pointcut("execution(* de.rieckpil.controllers.*.*(..))")
   public void forControllerMethods() {};
-  
+
   @Before("forControllerMethods()")
-  public void logBeforeAllMethodsInPackage() {
-    log.info("IMPORTANT!!! BEFORE: method in de.rieckpil.controllers.* called at: " + new Date().getTime());
+  public void logBeforeAllMethodsInPackage(JoinPoint joinPoint) {
+
+    MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+    
+    Object[] args = joinPoint.getArgs();
+    
+    for (Object argument : args) {
+      log.info(String.format("argument passed: '%s'", argument));
+    }
+
+    log.info(
+        String.format("IMPORTANT!!! BEFORE: method %s in de.rieckpil.controllers.* called at: %s",
+            methodSignature, new Date().getTime()));
   }
 }
