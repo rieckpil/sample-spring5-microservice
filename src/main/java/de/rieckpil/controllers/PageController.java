edad3893ee73281f6145e72a199ca1ab43cea26d
Philip Riecks <mail@philipriecks.de>
2018-01-15 05:00:36 +0100

    update country from and controller for sending form data

--- src/main/java/de/rieckpil/controllers/PageController.java
+++ src/main/java/de/rieckpil/controllers/PageController.java
@@ -1,8 +1,11 @@
 package de.rieckpil.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,30 +15,33 @@ import de.rieckpil.services.CountryService;
 @Controller
 public class PageController {
 
-  private CountryService countryService;
+	private CountryService countryService;
 
-  public PageController(CountryService countryService) {
-    this.countryService = countryService;
-  }
+	public PageController(CountryService countryService) {
+		this.countryService = countryService;
+	}
 
-  @RequestMapping(value = "/countryList", method = RequestMethod.GET)
-  public String getCountryListPage(Model model) {
+	@RequestMapping(value = "/countryList", method = RequestMethod.GET)
+	public String getCountryListPage(Model model) {
 
-    model.addAttribute("countries", countryService.getAllCountries());
+		model.addAttribute("countries", countryService.getAllCountries());
 
-    return "countryList";
-  }
+		return "countryList";
+	}
 
-  @RequestMapping(value = "/country/new", method = RequestMethod.GET)
-  public String createNewCountryForm(Model model) {
-    return "countryForm";
-  }
-  
-  @RequestMapping(value = "/country", method = RequestMethod.GET)
-  public String createNewCountry(@RequestBody CountryDTO countryToSave , Model model) {
-	  
-	  System.out.println(countryToSave.toString());
-	  
-    return "countryList";
-  }
+	@RequestMapping(value = "/country/new", method = RequestMethod.GET)
+	public String createNewCountryForm(Model model) {
+
+		model.addAttribute("country", new CountryDTO());
+
+		return "countryForm";
+	}
+
+	@RequestMapping(value = "/country", method = RequestMethod.POST)
+	public String createNewCountry(@Valid @ModelAttribute("country") CountryDTO countryToSave, BindingResult bindingResult) {
+
+		System.out.println(countryToSave.toString());
+
+		return "redirect:/countryList";
+	}
 }
