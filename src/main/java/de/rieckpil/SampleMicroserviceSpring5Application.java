64c9f77b31b4b56e00118cc37a59d90f987813c9
Philip Riecks <mail@philipriecks.de>
2018-01-22 07:06:23 +0100

    add PasswordEncoder bean to project

--- src/main/java/de/rieckpil/SampleMicroserviceSpring5Application.java
+++ src/main/java/de/rieckpil/SampleMicroserviceSpring5Application.java
@@ -2,11 +2,19 @@ package de.rieckpil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SampleMicroserviceSpring5Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SampleMicroserviceSpring5Application.class, args);
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncode() {
+      return new BCryptPasswordEncoder();
     }
 }
