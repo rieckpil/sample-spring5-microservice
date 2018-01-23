7da15d628bb10ef01d551a61a94a97876042f9d7
Philip Riecks <mail@philipriecks.de>
2018-01-23 05:22:23 +0100

    remove old .sql scripts

--- src/main/resources/scripts/create-postgres-database.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE sample_application_dev;
-CREATE DATABASE sample_application_prod;
-
-CREATE USER sample_dev_user WITH PASSWORD 'changeme';
-CREATE USER sample_dev_prod WITH PASSWORD 'changeme';
-
-
