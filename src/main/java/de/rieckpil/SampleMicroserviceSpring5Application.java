f52c24fb941674f2d2422efca827d17878d96fa8
Philip Riecks <mail@philipriecks.de>
2018-01-23 05:23:08 +0100

    override datasource bean to apply schema on startup

--- src/main/java/de/rieckpil/SampleMicroserviceSpring5Application.java
+++ src/main/java/de/rieckpil/SampleMicroserviceSpring5Application.java
@@ -1,8 +1,13 @@
 package de.rieckpil;
 
+import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,5 +21,15 @@ public class SampleMicroserviceSpring5Application {
     @Bean
     public PasswordEncoder passwordEncode() {
       return new BCryptPasswordEncoder();
+    }
+    
+    @Bean("dataSource")
+    @Profile("default")
+    @Primary
+    public DataSource standaloneDefaultDataSource() {
+      return new EmbeddedDatabaseBuilder()
+          .setType(EmbeddedDatabaseType.H2)
+          .addScript("classpath:scripts/h2-schema.sql")
+          .build();
     }
 }
