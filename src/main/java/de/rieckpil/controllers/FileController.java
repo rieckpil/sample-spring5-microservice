15733623c3ea0441dd484f44fee2be9dcd8abff7
Philip Riecks <mail@philipriecks.de>
2018-01-25 04:33:13 +0100

    update controller endpoint to use service

--- src/main/java/de/rieckpil/controllers/FileController.java
+++ src/main/java/de/rieckpil/controllers/FileController.java
@@ -8,12 +8,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import de.rieckpil.services.FileService;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/upload-file")
 @Slf4j
 public class FileController {
+
+  private final FileService fileService;
+
+  public FileController(FileService fileService) {
+    this.fileService = fileService;
+  }
 
   @GetMapping
   public String getUploadForm() {
@@ -24,15 +31,14 @@ public class FileController {
   public String storeUploadedFile(@RequestParam("name") String fileName,
       @RequestParam("file") MultipartFile file, Model model) throws IOException {
 
-    for (Byte fileByte : file.getBytes()) {
-      System.out.print(fileByte.byteValue() + " ");
-    }
-
     log.info(
         String.format("Uploaded file '%s' with name '%s' has size of %s bytes and is of type: %s",
             file.getOriginalFilename(), fileName, file.getSize(), file.getContentType()));
 
+    fileService.saveFile(fileName, file);
+    
     model.addAttribute("message", String.format("File '%s' was successfully uploaded!", fileName));
+    
     return "uploadForm";
 
   }
