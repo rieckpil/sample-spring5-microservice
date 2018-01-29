815b493268a4b03c0f66cae3e5b468bb6993ac79
Philip Riecks <philip@riecks.io>
2018-01-29 19:35:27 +0100

    update file storing logic to add parent folder if not existing

--- src/main/java/de/rieckpil/services/FileService.java
+++ src/main/java/de/rieckpil/services/FileService.java
@@ -1,10 +1,11 @@
 package de.rieckpil.services;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
-  public void saveFile(String name, MultipartFile file) throws IOException;
+  public void saveFile(String name, MultipartFile file) throws IOException, NoSuchAlgorithmException;
 
 }
--- src/main/java/de/rieckpil/services/FileServiceImpl.java
+++ src/main/java/de/rieckpil/services/FileServiceImpl.java
@@ -1,14 +1,18 @@
 package de.rieckpil.services;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import de.rieckpil.domain.File;
+import de.rieckpil.domain.UploadFile;
 import de.rieckpil.repositories.FileRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 public class FileServiceImpl implements FileService {
 
   private final FileRepository fileRepository;
@@ -20,23 +24,46 @@ public class FileServiceImpl implements FileService {
   }
 
   @Override
-  public void saveFile(String name, MultipartFile file) throws IOException {
+  public void saveFile(String name, MultipartFile file)
+      throws IOException, NoSuchAlgorithmException {
 
     if (file == null) {
       throw new RuntimeException("No file provided!");
     }
 
-    storeFileToDatabase(name, file);
-    storeFileInFilesystem(file);
+    if (!System.getProperty("os.name").contains("Windows")) {
+      storeFileToDatabase(name, file);
+    }
+
+    storeFileInFilesystem(file, "rieckpil");
 
   }
 
-  private void storeFileInFilesystem(MultipartFile file) throws FileNotFoundException, IOException {
-    FileOutputStream outputStream = new FileOutputStream(
-        STARTING_PATH + "/" + System.currentTimeMillis() + file.getOriginalFilename());
+  private void storeFileInFilesystem(MultipartFile file, String username)
+      throws FileNotFoundException, IOException, NoSuchAlgorithmException {
+
+    String folderName = "fileuploads-" + username;
+
+    createFolderIfNotExists(folderName);
+
+    FileOutputStream outputStream =
+        new FileOutputStream(STARTING_PATH + "/" + folderName + "/" + file.getOriginalFilename());
     byte[] fileBytes = file.getBytes();
     outputStream.write(fileBytes);
     outputStream.close();
+  }
+
+  private void createFolderIfNotExists(String folderName) {
+
+    File directory = new File(STARTING_PATH + "/" + folderName);
+
+    if (!directory.exists()) {
+      log.info(String.format(
+          "'%s' folder does not exists, creating the structure for storing uploaded files.",
+          folderName));
+      directory.mkdirs();
+    }
+
   }
 
   private void storeFileToDatabase(String name, MultipartFile file) throws IOException {
@@ -48,8 +75,9 @@ public class FileServiceImpl implements FileService {
       byteObjects[i++] = b;
     }
 
-    File fileToSave = File.builder().fileName(name).originalFileName(file.getOriginalFilename())
-        .contentType(file.getContentType()).size(file.getSize()).file(byteObjects).build();
+    UploadFile fileToSave =
+        UploadFile.builder().fileName(name).originalFileName(file.getOriginalFilename())
+            .contentType(file.getContentType()).size(file.getSize()).file(byteObjects).build();
 
     fileRepository.save(fileToSave);
   }
