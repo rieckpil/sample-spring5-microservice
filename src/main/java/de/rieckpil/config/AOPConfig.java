cecb9597c0464bbdcbd4fa39b7ba27d6a5fd88d0
Philip Riecks <philip@riecks.io>
2018-01-29 19:14:17 +0100

    enable aspects for dev profile

--- src/main/java/de/rieckpil/config/AOPConfig.java
+++ src/main/java/de/rieckpil/config/AOPConfig.java
@@ -3,9 +3,11 @@ package de.rieckpil.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan("de.rieckpil")
+@Profile({"dev"})
 public class AOPConfig {
 }
