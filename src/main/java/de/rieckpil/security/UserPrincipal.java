08b1d56181ebb5f5ba8dd14982b2df7b79b32fe3
Philip Riecks <philip@riecks.io>
2018-02-08 22:47:10 +0100

    update UserPrincipal to enable auth of self stored users

--- src/main/java/de/rieckpil/security/UserPrincipal.java
+++ src/main/java/de/rieckpil/security/UserPrincipal.java
@@ -31,17 +31,17 @@ public class UserPrincipal implements UserDetails {
 
   @Override
   public boolean isAccountNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isAccountNonLocked() {
-    return false;
+    return true;
   }
 
   @Override
   public boolean isCredentialsNonExpired() {
-    return false;
+    return true;
   }
 
   @Override
