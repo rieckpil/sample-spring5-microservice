1d0c8edec214ed0c0be56623e8183f13f37ae99b
Philip Riecks <philip@riecks.io>
2018-02-11 20:37:47 +0100

    Add RegistrationException

---
+++ src/main/java/com/rieckpil/exceptions/RegistrationException.java
@@ -0,0 +1,11 @@
+package com.rieckpil.exceptions;
+
+public class RegistrationException extends Exception{
+
+  private static final long serialVersionUID = 8490305623318258208L;
+
+  public RegistrationException(String message) {
+    super(message);
+  }
+
+}
