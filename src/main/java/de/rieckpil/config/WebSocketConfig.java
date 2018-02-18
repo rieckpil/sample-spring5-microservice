62178e6d43e17ba0e8154cada9bc7448a99a7b7b
Philip Riecks <philip@riecks.io>
2018-02-18 19:20:02 +0100

    add websocket configuration

---
+++ src/main/java/de/rieckpil/config/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package de.rieckpil.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/chat");
+  }
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry registry) {
+    registry.setApplicationDestinationPrefixes("/app");
+    registry.enableSimpleBroker("/topic");
+  }
+
+}
