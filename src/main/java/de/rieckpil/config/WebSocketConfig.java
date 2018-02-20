af33ab08837a312d160b9c8dc1051151715f5503
Philip Riecks <philip@riecks.io>
2018-02-20 06:21:40 +0100

    update WebSocketConfig

--- src/main/java/de/rieckpil/config/WebSocketConfig.java
+++ src/main/java/de/rieckpil/config/WebSocketConfig.java
@@ -2,15 +2,17 @@ package de.rieckpil.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/chat");
+    registry.addEndpoint("/ws").withSockJS();
   }
 
   @Override
