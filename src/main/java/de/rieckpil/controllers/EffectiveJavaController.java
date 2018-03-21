152e4cd440a851ab76fb371dbabc149302676e0b
Philip Riecks <rieckpil@schaeffler.com>
2018-03-21 07:56:40 +0100

    add registry metrics for counting the time of max integer sum

--- src/main/java/de/rieckpil/controllers/EffectiveJavaController.java
+++ src/main/java/de/rieckpil/controllers/EffectiveJavaController.java
@@ -1,5 +1,8 @@
 package de.rieckpil.controllers;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,27 +11,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/effective-java")
 public class EffectiveJavaController {
 
+  private Timer primitiveTimer;
+  private Timer regularTime;
+
+  public EffectiveJavaController(MeterRegistry registry) {
+    this.primitiveTimer = Timer.builder("dps.primitive.timer").description("Counting the time").tags("region", "time").register(registry);
+    this.regularTime = Timer.builder("dps.regular.timer").description("Counting the time").tags("region", "time").register(registry);
+  }
+
   @GetMapping("/primitive")
-  private Long getMaxIntSumWithPrimitiveLong() {
+  private Long getMaxIntSumWithPrimitiveLong() throws Exception {
 
-    Long sum = 0L;
+    Long sum = primitiveTimer.recordCallable(() -> {
+      Long innerSum = 0L;
+      for (int i = 0; i < Integer.MAX_VALUE; i++) {
+        innerSum += i;
+      }
+      return innerSum;
+    });
 
-    for (int i = 0; i < Integer.MAX_VALUE; i++) {
-      sum += i;
-    }
 
     return sum;
 
   }
 
   @GetMapping("/regular")
-  private long getMaxIntSumWithRegularLong() {
+  private long getMaxIntSumWithRegularLong() throws Exception {
 
-    long sum = 0;
-
-    for (int i = 0; i < Integer.MAX_VALUE; i++) {
-      sum += i;
-    }
+    long sum = regularTime.recordCallable(() -> {
+      long innerSum = 0L;
+      for (int i = 0; i < Integer.MAX_VALUE; i++) {
+        innerSum += i;
+      }
+      return innerSum;
+    });
 
     return sum;
 
