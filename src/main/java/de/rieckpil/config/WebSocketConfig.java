e1fe299b54eb7103b4b0cd6417ccce43d8f0cf24
Philip Riecks <philip@riecks.io>
2018-03-23 20:09:45 +0100

    add user endpoint for user specific websocket endpoints

--- src/main/java/de/rieckpil/config/WebSocketConfig.java
+++ src/main/java/de/rieckpil/config/WebSocketConfig.java
@@ -19,6 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     registry.setApplicationDestinationPrefixes("/app");
     registry.enableSimpleBroker("/topic");
+    registry.setUserDestinationPrefix("/user");
   }
 
 }
