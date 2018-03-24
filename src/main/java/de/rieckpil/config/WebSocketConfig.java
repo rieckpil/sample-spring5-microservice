2f81b369ececf09cf308197098d061dccdbee7f3
Philip Riecks <philip@riecks.io>
2018-03-24 10:16:53 +0100

    update WebSocketConfiguration

--- src/main/java/de/rieckpil/config/WebSocketConfig.java
+++ src/main/java/de/rieckpil/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
     registry.setApplicationDestinationPrefixes("/app");
-    registry.enableSimpleBroker("/topic");
+    registry.enableSimpleBroker("/topic", "/user");
     registry.setUserDestinationPrefix("/user");
   }
 
